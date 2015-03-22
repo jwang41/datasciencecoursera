664273b023ac2dafa6bdc3f5172691e55f908011
jwang41 <jwang41@gmail.com>
2015-03-22 16:55:13 -0500

    assignment2 for rprog

---
+++ assignment2.R
@@ -0,0 +1,28 @@
+#testing with B
+#B = matrix(c(2, 4, 3, 1, 5, 7,2,4,7), nrow=3, ncol=3) 
+#makeCacheMatrix <- function(x = B) {
+makeCacheMatrix <- function(x <-matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
